871b855561e595608ecb18dc0fb1cc7dbab0590f
AverageEnj0yer <112547632+AverageEnj0yer@users.noreply.github.com>
2022-12-24 21:44:23 +0300

    Add files via upload

---
+++ task6/Users.java
@@ -0,0 +1,11 @@
+package task6;
+
+public class Users {
+    String login, password;
+
+    public Users(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
+
